223951449903229aed67cfe3ef90aa55ce1b1c17
ArnDkl341232 <hungreman400@gmail.com>
2024-04-21 12:18:02 +0200

    21.04

--- board/__init__.py
+++ board/__init__.py
@@ -1,10 +1,22 @@
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def home():
+#     return "Hello world"
+#
+# if __name__  == "__main__":
+#     app.run(host = "0.0.0.0", port=8000, debug=True)
+
+
+
 from flask import Flask
+from board import pages
+from board import posts
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(pages.bp)
+    app.register_blueprint(posts.bp)
+    return app
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello world"
-
-if __name__  == "__main__":
-    app.run(host = "0.0.0.0", port=8000, debug=True)
---
+++ board/database.py
@@ -0,0 +1,5 @@
+import sqlite3
+from flask import current_app, g
+
+def init_app(app):
+    pass
