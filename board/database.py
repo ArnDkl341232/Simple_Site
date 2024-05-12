72c767214eb45072976505f906a3e10c2eec3583
ArnDkl341232 <hungreman400@gmail.com>
2024-05-12 12:14:48 +0200

    security improved + site update

--- board/database.py
+++ board/database.py
@@ -4,6 +4,7 @@ import click
 from flask import current_app, g
 
 def init_app(app):
+    app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
 
 @click.command("init-db")
@@ -23,3 +24,9 @@ def get_db():
         )
         g.db.row_factory = sqlite3.Row
     return g.db
+
+def close_db(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
--- board/posts.py
+++ board/posts.py
@@ -1,12 +1,29 @@
-from flask import Blueprint,render_template
+from flask import (Blueprint, render_template, request, redirect, url_for)
+from board.database import get_db
 
 bp = Blueprint("posts", __name__)
 
 @bp.route("/create",methods = ("GET" , "POST"))
 def create():
+    if request.method == "POST":
+        author = request.form["author"] or "NoName"
+        message = request.form["message"]
+
+        if message:
+            db = get_db()
+            db.execute("INSERT INTO post (author, message) VALUES (?, ?)",
+                       (author, message)
+                       )
+            db.commit()
+            return redirect(url_for("posts.posts"))
+
+
     return render_template("posts/create.html")
 
 @bp.route("/posts")
 def posts():
-    posts = []
+    db = get_db()
+    posts = db.execute(
+        "SELECT author, message, created FROM post ORDER BY created DESC"
+    )
     return render_template("posts/posts.html", posts = posts)
